327dbf696e2afc9a655cbd025c6e366aa7d99b30
Lucas Krause <lucas.rksr@gmail.com>
2022-03-10 19:31:52 -0300

    Update 02-listas.py

--- 02-listas.py
+++ 02-listas.py
@@ -6,20 +6,20 @@
 # >>> Elemento 6 na posição 2
 # >>> Elemento “H” na posição 3
 
-# lista = [1, 2, 3, 4, 5]
+lista = [1, 2, 3, 4, 5]
 
-# print(f"Elemento {lista[0]} na posição {lista.index(1)}\nElemento {lista[1]} na posição {lista.index(2)}\nElemento {lista[2]} na posição {lista.index(3)}\nElemento {lista[3]} na posição {lista.index(4)}\nElemento {lista[4]} na posição {lista.index(5)}\n")
+print(f"Elemento {lista[0]} na posição {lista.index(1)}\nElemento {lista[1]} na posição {lista.index(2)}\nElemento {lista[2]} na posição {lista.index(3)}\nElemento {lista[3]} na posição {lista.index(4)}\nElemento {lista[4]} na posição {lista.index(5)}\n")
 
 # Exercício 2:
 
 # Crie uma lista com 10 elementos (você escolhe quais serão) e imprima a lista na ordem inversa. Exemplo: para a lista [1, 3, 6, “H”, [7,7,7] a saída deve ser
 # >>> [[7,7,7], “H”, 6, 3, 1]
 
-# lista_normal = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+lista_normal = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# lista_normal.reverse()
+lista_normal.reverse()
 
-# print(lista_normal)
+print(lista_normal)
 
 
 # Exercício 3:
@@ -30,17 +30,17 @@
 
 # Obs: caso o maior ou o menor número sejam repetidos, trazer a menor posição.
 
-# lista = [3, 1, 4, 7, 5, 6]
+lista = [3, 1, 4, 7, 5, 6]
 
-# maior_elemento = max(lista)
+maior_elemento = max(lista)
 
-# menor_elemento = min(lista)
+menor_elemento = min(lista)
 
-# lista.index(maior_elemento)
+lista.index(maior_elemento)
 
-# lista.index(menor_elemento)
+lista.index(menor_elemento)
 
-# print(f"O maior elemento é {maior_elemento} e está na posição {lista.index(maior_elemento)}\nO menor elemento é {menor_elemento} e está na posição {lista.index(menor_elemento)}")
+print(f"O maior elemento é {maior_elemento} e está na posição {lista.index(maior_elemento)}\nO menor elemento é {menor_elemento} e está na posição {lista.index(menor_elemento)}")
 
 # Exercício 4:
 
@@ -50,60 +50,60 @@
 # >>> 3 – março
 # >>> 6 – junho
 
-# jan = float(input("Temperatura de janeiro: "))
-# fev = float(input("Temperatura de fevereiro: "))
-# mar = float(input("Temperatura de março: "))
-# abr = float(input("Temperatura de abril: "))
-# mai = float(input("Temperatura de maio: "))
-# jun = float(input("Temperatura de junho: "))
-# jul = float(input("Temperatura de julho: "))
-# ago = float(input("Temperatura de agosto: "))
-# set = float(input("Temperatura de setembro: "))
-# out = float(input("Temperatura de outubro: "))
-# nov = float(input("Temperatura de novembro: "))
-# dez = float(input("Temperatura de dezembro: "))
+jan = float(input("Temperatura de janeiro: "))
+fev = float(input("Temperatura de fevereiro: "))
+mar = float(input("Temperatura de março: "))
+abr = float(input("Temperatura de abril: "))
+mai = float(input("Temperatura de maio: "))
+jun = float(input("Temperatura de junho: "))
+jul = float(input("Temperatura de julho: "))
+ago = float(input("Temperatura de agosto: "))
+set = float(input("Temperatura de setembro: "))
+out = float(input("Temperatura de outubro: "))
+nov = float(input("Temperatura de novembro: "))
+dez = float(input("Temperatura de dezembro: "))
 
-# lista_temp = [jan, fev, mar, abr, mai, jun, jul, ago, set, out, nov, dez]
+lista_temp = [jan, fev, mar, abr, mai, jun, jul, ago, set, out, nov, dez]
 
-# media_temp = sum(lista_temp) / len(lista_temp)
+media_temp = sum(lista_temp) / len(lista_temp)
 
-# print(f"Meses com temperatura acima da média anual de {media_temp}°")
+print(f"Meses com temperatura acima da média anual de {media_temp}°")
 
-# if jan > media_temp:
-#     print(f"{jan} - janeiro")
+if jan > media_temp:
+    print(f"{jan} - janeiro")
 
-# if fev > media_temp:
-#     print(f"{fev} - fevereiro")
+if fev > media_temp:
+    print(f"{fev} - fevereiro")
 
-# if mar > media_temp:
-#     print(f"{mar} - março")
+if mar > media_temp:
+    print(f"{mar} - março")
 
-# if abr > media_temp:
-#     print(f"{abr} - abril")
+if abr > media_temp:
+    print(f"{abr} - abril")
 
-# if mai > media_temp:
-#     print(f"{mai} - maio")
+if mai > media_temp:
+    print(f"{mai} - maio")
 
-# if jun > media_temp:
-#     print(f"{jun} - junho")
+if jun > media_temp:
+    print(f"{jun} - junho")
 
-# if jul > media_temp:
-#     print(f"{jul} - julho")
+if jul > media_temp:
+    print(f"{jul} - julho")
 
-# if ago > media_temp:
-#     print(f"{ago} - agosto")
+if ago > media_temp:
+    print(f"{ago} - agosto")
 
-# if set > media_temp:
-#     print(f"{set} - setembro")
+if set > media_temp:
+    print(f"{set} - setembro")
 
-# if out > media_temp:
-#     print(f"{out} - outubro")
+if out > media_temp:
+    print(f"{out} - outubro")
 
-# if nov > media_temp:
-#     print(f"{nov} - novembro")
+if nov > media_temp:
+    print(f"{nov} - novembro")
 
-# if dez > media_temp:
-#     print(f"{dez} - dezembro")
+if dez > media_temp:
+    print(f"{dez} - dezembro")
 
 
 # Exercício 5:
@@ -111,33 +111,33 @@
 # Dada a seguinte lista lst = [10, 20, [300, 400, [5000, 6000], 500], 30, 40] adicione o elemento 7000 logo após o elemento 6000 na lista acima. O resultado final deverá ser:
 # >>> [10, 20, [300, 400, [5000, 6000, 7000], 500], 30, 40]
 
-# lst = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+lst = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
 
-# lst2 = lst.pop(2)
+lst2 = lst.pop(2)
 
-# lst3 = lst2.pop(2)
+lst3 = lst2.pop(2)
 
-# lst3.append(7000)
+lst3.append(7000)
 
-# lst2.insert(2, lst3)
+lst2.insert(2, lst3)
 
-# lst.insert(2, lst2)
+lst.insert(2, lst2)
 
-# print(lst)
+print(lst)
 
 # Exercício 6:
 
 # Faça um programa que remova strings vazias de uma lista de strings. Exemplo: dada a lista [“Olá”, “”, “meu”, “nome”, “”, “é”, “facilitador”, “”] a saída deve ser
 # >>> [“Olá”, “meu”, “nome”, “é”, “facilitador”]
 
-# lista = ["Olá", "", "meu", "nome", "", "é", "facilitador", ""]
+lista = ["Olá", "", "meu", "nome", "", "é", "facilitador", ""]
 
-# qtd_vazios = lista.count("")
+qtd_vazios = lista.count("")
 
-# for i in range(qtd_vazios):
-#     lista.remove("")
+for i in range(qtd_vazios):
+    lista.remove("")
 
-# print(lista)
+print(lista)
 
 # Exercício 7:
 
@@ -145,14 +145,14 @@
 
 # Faça também o inverso, dada a mesma lista só que agora de elementos inteiros, converta todos os elementos para int.
 
-# lista1 = ["1", "7", "99", "15"]
+lista1 = ["1", "7", "99", "15"]
 
-# lista1_int = [int(i) for i in lista1]
+lista1_int = [int(i) for i in lista1]
 
-# print(lista1_int)
+print(lista1_int)
 
-# lista2 = [1, 7, 99, 15]
+lista2 = [1, 7, 99, 15]
 
-# lista2_str = [str(i) for i in lista2]
+lista2_str = [str(i) for i in lista2]
 
-# print(lista2_str)
+print(lista2_str)
