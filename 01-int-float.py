bfb9556c628f24438bbed7a0c0635acea4e007d6
Lucas Krause <lucas.rksr@gmail.com>
2022-03-10 19:36:30 -0300

    Update 01-int-float.py

--- 01-int-float.py
+++ 01-int-float.py
@@ -15,27 +15,27 @@
 # >>> log10(100)
 # 2.0
 
-# A = int(input("Digite um número inteiro: "))
+A = int(input("Digite um número inteiro: "))
 
-# B = int(input("Digite um número inteiro: "))
+B = int(input("Digite um número inteiro: "))
 
-# soma = A + B
+soma = A + B
 
-# diferenca = A - B
+diferenca = A - B
 
-# produto = A * B
+produto = A * B
 
-# quociente = A // B
+quociente = A // B
 
-# resto = A % B
+resto = A % B
 
-# from math import log10
+from math import log10
 
-# resultado_log = log10(A)
+resultado_log = log10(A)
 
-# resultado_exp = A ** B
+resultado_exp = A ** B
 
-# print(f"O resultado da soma é: {soma}.\nO resultado da diferença é: {diferenca}.\nO resultado da multiplicação é: {produto}.\nO quociente da divisão é: {quociente}.\nO resto da divisão é: {resto}.\nO resultado do log10 de A é: {resultado_log}.\nO resultado da exponenciação é: {resultado_exp}.")
+print(f"O resultado da soma é: {soma}.\nO resultado da diferença é: {diferenca}.\nO resultado da multiplicação é: {produto}.\nO quociente da divisão é: {quociente}.\nO resto da divisão é: {resto}.\nO resultado do log10 de A é: {resultado_log}.\nO resultado da exponenciação é: {resultado_exp}.")
 
 # Exercício 2:
 
@@ -45,15 +45,15 @@
 # A área de um triângulo é dada pela seguinte fórmula abaixo
 # area=(base x altura)/2
 
-# base = float(input("Digite o valor da base do triângulo: "))
+base = float(input("Digite o valor da base do triângulo: "))
 
-# altura = float(input("Digite o valor da base do triângulo: "))
+altura = float(input("Digite o valor da base do triângulo: "))
 
-# unidade_medida = input("Digite a unidade de medida dos valores indicados do triângulo: ")
+unidade_medida = input("Digite a unidade de medida dos valores indicados do triângulo: ")
 
-# area = (base * altura) / 2
+area = (base * altura) / 2
 
-# print(f"A área do triângulo é de {area} {unidade_medida}².")
+print(f"A área do triângulo é de {area} {unidade_medida}².")
 
 # Exercício 3:
 
@@ -65,21 +65,21 @@
 # s=(s1+s2+s3)/2
 # area=√(s.(s-s1).(s-s2).(s-s3))
 
-# lado1 = float(input("Digite o valor do lado1 do triângulo: "))
+lado1 = float(input("Digite o valor do lado1 do triângulo: "))
 
-# lado2 = float(input("Digite o valor do lado2 do triângulo: "))
+lado2 = float(input("Digite o valor do lado2 do triângulo: "))
 
-# lado3 = float(input("Digite o valor do lado3 do triângulo: "))
+lado3 = float(input("Digite o valor do lado3 do triângulo: "))
 
-# unidade_medida = input("Digite a unidade de medida dos valores indicados do triângulo: ")
+unidade_medida = input("Digite a unidade de medida dos valores indicados do triângulo: ")
 
-# s = (lado1 + lado2 + lado3) / 2
+s = (lado1 + lado2 + lado3) / 2
 
-# from math import isqrt
+from math import isqrt
 
-# area = isqrt(int(s * (s - lado1) * (s - lado2) * (s - lado3)))
+area = isqrt(int(s * (s - lado1) * (s - lado2) * (s - lado3)))
 
-# print(f"A área do triângulo é de {area} {unidade_medida}².")
+print(f"A área do triângulo é de {area} {unidade_medida}².")
 
 # Exercício 4:
 
@@ -89,22 +89,22 @@
 # O IMC é calculado de acordo com a fórmula abaixo
 # IMC=peso/altura^2
 
-# peso = float(input("Digite seu peso em kg: "))
+peso = float(input("Digite seu peso em kg: "))
 
-# altura = float(input("Digite sua altura em metros: "))
+altura = float(input("Digite sua altura em metros: "))
 
-# imc = peso / (altura ** 2)
+imc = peso / (altura ** 2)
 
-# print(f"Seu IMC é {imc}.")
+print(f"Seu IMC é {imc}.")
 
 # Exercício 5:
 
 # Escreva um programa que leia do usuário um número de 4 dígitos e imprima a soma destes dígitos. Exemplo, se o usuário digitar 3141 seu programa deverá imprimir na tela 3+1+4+1=9.
 
-# numero = input("Digite um número inteiro com 4 dígitos: ")
+numero = input("Digite um número inteiro com 4 dígitos: ")
 
-# digitos = [int(x) for x in str(numero)]
+digitos = [int(x) for x in str(numero)]
 
-# soma = int(sum(digitos))
+soma = int(sum(digitos))
 
-# print(f"O somatório dos dígitos de {numero} é {digitos[0]} + {digitos[1]} + {digitos[2]} + {digitos[3]} = {soma}.")
+print(f"O somatório dos dígitos de {numero} é {digitos[0]} + {digitos[1]} + {digitos[2]} + {digitos[3]} = {soma}.")
